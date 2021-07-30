81f4127e6bd2d8bbd64a14c378602a2e77e67d24
FedericoVigi <federico10101@gmail.com>
2021-07-30 13:03:02 -0300

    fix inputs

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 (function () {
 
     let simulateBtn = document.getElementById("simulate-btn");
-    //let weekAgentsInput = document.getElementById('weekAgents')
-    //let weekendAgentsInput = document.getElementById('weekendAgents')
+    let weekAgentsInput = document.getElementById('weekAgents')
+    let weekendAgentsInput = document.getElementById('weekendAgents')
 
     simulateBtn.onclick = function () {
         
-        //let weekAgents = parseInt(weekAgentsInput.value)
-        //let weekendAgents = parseInt(weekendAgentsInput.value)
+        let weekAgents = parseInt(weekAgentsInput.value)
+        let weekendAgents = parseInt(weekendAgentsInput.value)
 
+        if(isNaN(weekAgents) || isNaN(weekendAgents)){
+            alert("Entrada Invalida")
+        }
+        console.log(weekAgents)
+        console.log(weekendAgents)
         //results = simulate(165,145)// pesimista
-        results = simulate(100,80)// real
+        //results = simulate(100,80)// real
         //results = simulate(190,160) //  optimo
+        results = simulate(weekAgents,weekendAgents)
         generateGraph(results)
     };
 
